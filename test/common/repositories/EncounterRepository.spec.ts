9b274fe4ea27701907c531aa9eac47aa8de6c2a5
marvin bustamante <vinbustamante@gmail.com>
2019-07-21 22:45:29 +0800

    add unit test for EncounterRepository

---
+++ test/common/repositories/EncounterRepository.spec.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata';
+import * as expect from 'expect';
+import * as sinon from 'sinon';
+import { EncounterRepository } from '../../../src/common/repositories/implementation/EncounterRepository';
+
+describe('EncounterRepository', () => {
+    let _encounterRepository;
+
+    beforeEach(() => {
+        _encounterRepository = new EncounterRepository();
+        sinon.stub(_encounterRepository, 'request');
+    });
+
+    describe('getById()', () => {
+        it('it should call back request method', async () => {
+            const id = 1;
+            await _encounterRepository.getById(id);
+            expect(_encounterRepository.request.callCount).toBe(1);
+            const arg = _encounterRepository.request.getCall(0).args[0];
+            expect(arg.url).toBe(`/pokemon/${id}/encounters`);
+            expect(arg.method).toBe('GET');
+        });
+    });
+
+});
